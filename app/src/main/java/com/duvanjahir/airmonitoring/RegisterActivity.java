e41143cbc2b55304fd57c781c0745462c92a9228
harvey <harvey.lombara@gmail.com>
2021-10-29 01:53:14 -0500

    Registro Clientes

--- app/src/main/java/com/duvanjahir/airmonitoring/RegisterActivity.java
+++ app/src/main/java/com/duvanjahir/airmonitoring/RegisterActivity.java
@@ -95,7 +95,6 @@ public class RegisterActivity extends AppCompatActivity {
                             }
                         });
                     }
-
                 }
             }
         });
