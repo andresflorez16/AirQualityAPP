410c071c4cee80df9713a7d6807d2dc0c7b33ba4
andresflorez16 <andresflorez.dev@gmail.com>
2021-10-29 01:24:25 -0500

    project

--- app/src/main/java/com/duvanjahir/airmonitoring/MainActivity.java
+++ app/src/main/java/com/duvanjahir/airmonitoring/MainActivity.java
@@ -2,24 +2,48 @@ package com.duvanjahir.airmonitoring;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hanks.htextview.HTextView;
 import com.hanks.htextview.HTextViewType;
 
 public class MainActivity extends AppCompatActivity {
 
-    private HTextView hTextView;
+    /*private HTextView hTextView;*/
+    private EditText emailText, passwordText;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hTextView = (HTextView) findViewById(R.id.txtRegistrar);
+        /*hTextView = findViewById(R.id.txtRegistrar);
         hTextView.setTextColor(Color.WHITE);
         hTextView.setBackgroundColor(Color.BLACK);
         //hTextView.setTypeface(FontManager.getInstance(getAssets()).getFont("font/Caveat-VariableFont_wght.ttf"));
-        hTextView.setAnimateType(HTextViewType.LINE);
+        hTextView.setAnimateType(HTextViewType.LINE);*/
+
+        emailText = (EditText) findViewById(R.id.ingreso_email);
+        passwordText = (EditText) findViewById(R.id.ingreso_password);
+
     }
+
+    public void btnRegister(View view) {
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        System.out.println(email + " " + password);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/duvanjahir/airmonitoring/RegisterActivity.java
+++ app/src/main/java/com/duvanjahir/airmonitoring/RegisterActivity.java
@@ -3,12 +3,44 @@ package com.duvanjahir.airmonitoring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView userType;
+    private EditText nameText, passwordText, passwordVerify, emailText, phoneText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, USERS);
+        userType = (AutoCompleteTextView) findViewById(R.id.registro_tipo);
+        userType.setAdapter(adapter);
+        nameText = (EditText) findViewById(R.id.registro_nombre);
+        passwordText = (EditText) findViewById(R.id.registro_password);
+        passwordVerify = (EditText) findViewById(R.id.registro_confirmacionpassword);
+        phoneText = (EditText) findViewById(R.id.registro_telefono);
+
+
+    }
+    private static final String[] USERS = new String[] {
+            "cliente", "tecnico"
+    };
+
+    public void btnRegister(View view) {
+        final int[] pos = new int[0];
+        userType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                pos[0] = i;
+            }
+        });
+        Toast.makeText(this, pos[0], Toast.LENGTH_SHORT).show();
     }
 }
