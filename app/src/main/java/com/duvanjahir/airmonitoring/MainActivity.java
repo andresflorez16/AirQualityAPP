df6ada0e001c5a6546741e577453dd4a903ec6cc
harvey <harvey.lombara@gmail.com>
2021-10-29 01:19:59 -0500

    Registro Clientes

--- app/src/main/java/com/duvanjahir/airmonitoring/MainActivity.java
+++ app/src/main/java/com/duvanjahir/airmonitoring/MainActivity.java
@@ -2,24 +2,29 @@ package com.duvanjahir.airmonitoring;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.hanks.htextview.HTextView;
-import com.hanks.htextview.HTextViewType;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    private HTextView hTextView;
+    private EditText email,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        hTextView = (HTextView) findViewById(R.id.txtRegistrar);
-        hTextView.setTextColor(Color.WHITE);
-        hTextView.setBackgroundColor(Color.BLACK);
-        //hTextView.setTypeface(FontManager.getInstance(getAssets()).getFont("font/Caveat-VariableFont_wght.ttf"));
-        hTextView.setAnimateType(HTextViewType.LINE);
+        email=(EditText) findViewById(R.id.ingreso_email);
+        pass=(EditText) findViewById(R.id.ingreso_password);
+    }
+
+    public void ingresar(View view){
+        String Emai=email.getText().toString();
+        String Pass=pass.getText().toString();
+        System.out.println(Emai+" "+Pass);
+    }
+    public void Registarse(View view){
+        Intent intent= new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/duvanjahir/airmonitoring/RegisterActivity.java
+++ app/src/main/java/com/duvanjahir/airmonitoring/RegisterActivity.java
@@ -1,14 +1,113 @@
 package com.duvanjahir.airmonitoring;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private AutoCompleteTextView Typeuser;
+    private EditText nameText,passwordText,passwordVerifyText,emailText,phoneText;
+    FirebaseFirestore db;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, users);
+        //Typeuser = (AutoCompleteTextView) findViewById(R.id.registro_tipo);
+        //Typeuser.setAdapter(adapter);
+        nameText=(EditText) findViewById(R.id.registro_nombre);
+        passwordText=(EditText) findViewById(R.id.registro_password);
+        passwordVerifyText=(EditText) findViewById(R.id.registro_confirmacionpassword);
+        emailText=(EditText) findViewById(R.id.registro_email);
+        phoneText=(EditText) findViewById(R.id.registro_telefono);
+        initialize();
     }
+    public  void  initialize(){
+        FirebaseApp.initializeApp(this);
+        db=FirebaseFirestore.getInstance();
+    }
+    public void Registrar(View view){
+
+        String name = nameText.getText().toString();
+        String password = passwordText.getText().toString();
+        String passwordV = passwordVerifyText.getText().toString();
+        String email = emailText.getText().toString();
+        String phone = phoneText.getText().toString();
+        //System.out.println(NomApe + " " + Con + " " + Confcon + " " + email + " " + Tele);
+
+        if(name.equals("")||password.equals("")||passwordV.equals("")||email.equals("")||phone.equals("")){
+            Toast.makeText(this, "Verifique los campos vacios", Toast.LENGTH_SHORT).show();
+        }else{
+            if(password.equals(passwordV)) {
+                User user= new User(name,email,phone,password,"cliente");
+                insertUser(view,user);
+            }else{
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
+    public  void  insertUser(View view, User myUser){
+        HashMap<String,Object>user= new HashMap<>();
+        user.put("name",myUser.getName());
+        user.put("email",myUser.getEmail());
+        user.put("password",myUser.getPassword());
+        user.put("phone",myUser.getPhone());
+        user.put("type",myUser.getType());
+        DocumentReference docRef =db.collection("users").document(myUser.getEmail());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot document= task.getResult();
+                    if(document.exists()){
+                        Toast.makeText(getApplicationContext(), "El email ya se encuentra registrado", Toast.LENGTH_SHORT).show();
+                    }else{
+                        db.collection("users").document(myUser.getEmail()).set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(getApplicationContext(), "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(), "Ocurrió un error", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                }
+            }
+        });
+    }
+
+
+//    private static final String[] users = new String[] {
+//            "cliente", "tecnico"
+//    };
+
 }
+
+
+
+
